0e561dc6cc292fa1348c583437cdd06bc7c5e83f
chrissyhunt87 <chrissy.hunt@gmail.com>
2017-12-29 09:09:43 -0500

    Build login and logout functions.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,7 +14,7 @@ class UserController < ApplicationController
   end
 
   post '/users/signup' do
-    if params[:user][:name] != "" && params[:user][:email] != "" && params[:user][:password] != "" &&
+    if params[:user][:name] != "" && params[:user][:email] != "" && params[:user][:password] != "" && !User.find_by(email: params[:email][:password])
       new_user = User.create(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
       if new_user.save
         session[:user_id] = new_user.id
@@ -31,9 +31,41 @@ class UserController < ApplicationController
     erb :'/users/login'
   end
 
+  post '/users/login' do
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect to '/users'
+    else
+      redirect to '/users/login'
+    end
+  end
+
   get '/users/edit' do
-    @user = current_user
-    erb :'/users/edit'
+    if logged_in?
+      @user = current_user
+      erb :'/users/edit'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  post '/users/edit' do
+    if params[:user][:name] != "" && params[:user][:email] != "" && params[:user][:password] != ""
+      @user = current_user
+      @user.name = params[:user][:name]
+      @user.email = params[:user][:email]
+      @user.password = params[:user][:password]
+      @user.save
+      redirect to '/users'
+    else
+      redirect to '/users/edit'
+    end
+  end
+
+  get '/users/logout' do
+    session.clear
+    redirect to '/users/login'
   end
 
 end
