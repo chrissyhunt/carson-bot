14c1cb7b55f8b8a63ee708e91fb3b3f733a9d6f0
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-01-06 10:01:55 -0500

    Fixed signup redirect, added flash error messages

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
     end
 
     @user = current_user
-    erb :'/users/index'
+    erb :'/users/show'
   end
 
   get '/users/signup' do
@@ -18,14 +18,31 @@ class UserController < ApplicationController
   end
 
   post '/users/signup' do
-    if params[:user][:name] != "" && params[:user][:email] != "" && params[:user][:password] != "" && !User.find_by(email: params[:user][:email])
-      new_user = User.create(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
-      if new_user.save
-        session[:user_id] = new_user.id
-        redirect to '/users'
-      else
-        redirect to '/signup'
-      end
+    if params[:user][:name] == "" || params[:user][:name] == " " || params[:user][:name].blank?
+      flash[:message] = "You must provide a name."
+      redirect to '/users/signup'
+    end
+
+    if params[:user][:email] == "" || params[:user][:email] == " " || params[:user][:email].blank?
+      flash[:message] = "You must provide a valid email address."
+      redirect to '/users/signup'
+    end
+
+    if params[:user][:password] == "" || params[:user][:password] == " " || params[:user][:password].blank?
+      flash[:message] = "You must select a password."
+      redirect to '/users/signup'
+    end
+
+    if User.find_by(email: params[:user][:email])
+    flash[:message] = "An account already exists for that email address.<br> Please log in or choose a different email."
+      redirect to '/users/signup'
+    end
+
+    new_user = User.create(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
+
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect to '/users'
     else
       redirect to '/signup'
     end
@@ -35,7 +52,7 @@ class UserController < ApplicationController
     if logged_in?
       redirect to '/users'
     end
-    
+
     erb :'/users/login'
   end
 
@@ -45,6 +62,7 @@ class UserController < ApplicationController
       session[:user_id] = user.id
       redirect to '/users'
     else
+      flash[:message] = "I'm sorry, that's not the right password. Please try again."
       redirect to '/users/login'
     end
   end
@@ -67,6 +85,7 @@ class UserController < ApplicationController
       @user.save
       redirect to '/users'
     else
+      flash[:message] = "You must provide a name, email address, and password."
       redirect to '/users/edit'
     end
   end
